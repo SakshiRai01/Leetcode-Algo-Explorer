8a3e667c7eb0aad3d0a91971cc60ff4c35142741
Sakshi Singh <92785809+SakshiRai01@users.noreply.github.com>
2023-08-21 23:47:36 +0530

    Time: 3 ms (25.89%) | Memory: 6 MB (21.63%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        if(num == 1)
+        return 0;
+        if(num == 0 )
+        return 1;
+
+        vector<int> v;
+        while(num!=0)
+        {
+            v.push_back(num%2);
+            num/=2;
+        }
+
+        for(int i=0 ; i<v.size(); i++)
+        {
+            if(v[i]==0)
+            v[i]=1;
+            else
+            v[i]=0;
+        }
+
+        long long int ans = 1;
+
+        for(int i=0 ; i<v.size() ; i++)
+        {
+            num += v[i]*ans;
+            ans *= 2;
+        }
+        return num;
+    }
+};
