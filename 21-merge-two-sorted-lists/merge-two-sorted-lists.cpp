03629e895e577428594b41932ff5abc2de379ecd
Sakshi Singh <92785809+SakshiRai01@users.noreply.github.com>
2024-03-04 20:26:19 +0530

    Time: 5 ms (50.27%) | Memory: 18.3 MB (30.49%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,58 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* f, ListNode* s)
-    {
-        if( f -> next == NULL)
-        {
-            f -> next = s;
-            return f;
-        }
-
-        ListNode* curr1 = f;
-        ListNode* next1 = curr1 -> next;
-        ListNode* curr2 = s;
-        ListNode* next2 = curr2 -> next;
-
-        while(next1 != NULL && curr2 != NULL)
-        {
-            if(curr2 -> val >= curr1 -> val && curr2 -> val <= next1 -> val)
-            {
-                curr1 -> next = curr2;
-                next2 = curr2 -> next;
-                curr2 -> next = next1;
-
-                curr1 = curr2;
-                curr2 = next2;
-            }
-
-            else
-            {
-                curr1 = next1;
-                next1 = next1 -> next;
-
-                if(next1 == NULL)
-                {
-                    curr1 -> next = curr2;
-                    return f;
-                }
-            }
-        }
-        return f;
-    }
-    
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1 == NULL)
         return list2;
         if(list2 == NULL)
         return list1;
-        ListNode* l = NULL;
 
-        if(list1 -> val <= list2 -> val)
-        l = solve(list1 , list2);
-        else
-        l = solve(list2 , list1);
+        ListNode* ans = new ListNode(-1);
+        ListNode* head = ans;
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        while(temp1 && temp2)
+        {
+            if(temp1 -> val <= temp2 -> val)
+            {
+                head -> next = temp1;
+                head = head -> next;
+                temp1 = temp1 -> next;
+            }
 
-        return l;
+            else if(temp1 -> val >= temp2 -> val)
+            {
+                head -> next = temp2;
+                head = head -> next;
+                temp2 = temp2 -> next;
+            }
+           
+        }
+         while(temp1)
+            {
+                head -> next = temp1;
+                head = head -> next;
+                temp1 = temp1 -> next;
+            }
+
+            while(temp2)
+            {
+                head -> next = temp2;
+                head = head -> next;
+                temp2 = temp2 -> next;
+            }
+
+        return ans -> next;
     }
 };
